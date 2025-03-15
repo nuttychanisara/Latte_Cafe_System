b7cde843cdd25dc56be4612d2a37363b62a2ee86
Chanisara Thippan <101926514+nuttychanisara@users.noreply.github.com>
2025-03-15 16:54:46 +0700

    Add files via upload

---
+++ Group5_LatteCafe/LatteCafe/formCash.Designer.cs
@@ -0,0 +1,132 @@
+﻿namespace LatteCafe
+{
+    partial class formCash
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.textTotal = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textMoney = new System.Windows.Forms.TextBox();
+            this.textChange = new System.Windows.Forms.TextBox();
+            this.buttonPrint = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(156, 78);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(74, 25);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "ราคารวม";
+            // 
+            // textTotal
+            // 
+            this.textTotal.Location = new System.Drawing.Point(275, 73);
+            this.textTotal.Name = "textTotal";
+            this.textTotal.Size = new System.Drawing.Size(138, 30);
+            this.textTotal.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(180, 134);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(50, 25);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "รับมา";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(159, 194);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(71, 25);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "เงินทอน";
+            // 
+            // textMoney
+            // 
+            this.textMoney.Location = new System.Drawing.Point(275, 129);
+            this.textMoney.Name = "textMoney";
+            this.textMoney.Size = new System.Drawing.Size(138, 30);
+            this.textMoney.TabIndex = 1;
+            // 
+            // textChange
+            // 
+            this.textChange.Location = new System.Drawing.Point(275, 189);
+            this.textChange.Name = "textChange";
+            this.textChange.Size = new System.Drawing.Size(138, 30);
+            this.textChange.TabIndex = 1;
+            // 
+            // buttonPrint
+            // 
+            this.buttonPrint.Location = new System.Drawing.Point(563, 250);
+            this.buttonPrint.Name = "buttonPrint";
+            this.buttonPrint.Size = new System.Drawing.Size(120, 43);
+            this.buttonPrint.TabIndex = 2;
+            this.buttonPrint.Text = "ใบเสร็จ";
+            this.buttonPrint.UseVisualStyleBackColor = true;
+            // 
+            // formCash
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(734, 316);
+            this.Controls.Add(this.buttonPrint);
+            this.Controls.Add(this.textChange);
+            this.Controls.Add(this.textMoney);
+            this.Controls.Add(this.textTotal);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(222)));
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Name = "formCash";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "formCash";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textTotal;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textMoney;
+        private System.Windows.Forms.TextBox textChange;
+        private System.Windows.Forms.Button buttonPrint;
+    }
+}
